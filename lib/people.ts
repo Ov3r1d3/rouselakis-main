65c12da4d867a19963aa39790b8e96071327aeac
Rouselakis Stelios <stelios.rouselakis@upstreamsystems.com>
2024-01-30 23:25:12 +0200

    Added static files

--- lib/people.ts
+++ lib/people.ts
@@ -1,15 +1,29 @@
-import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faGitAlt, faGithub, faInstagram, faInstagramSquare, faLinkedin, faLinkedinIn, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 export const SteliosRouselakis = {
     name: 'Rouselakis Stelios',
     jobTitle: 'Software Development Tech Lead',
-    websiteUrl: 's.rouselakis.com',
+    websiteUrl: 'https://s.rouselakis.com',
     description: "Software Development Tech Lead with a background in Electrical Engineering. \nGraduating with a degree in Electrical Engineering, transitioned into freelancing, specializing in website construction and developing automation solutions.\nDedicated to software development, progressively taking on more responsibility.\nCurrently, leading a dynamic developer team, leveraging my technical expertise and leadership skills to drive projects forward and achieve exceptional results.",
     socials: [
         {
-            icon: faLinkedinIn,
+            icon: faLinkedin,
             link: 'https://www.linkedin.com/in/rouselakis/',
-            id: 'linked-in'
+            id: 's-linked-in'
+        },
+        {
+            icon: faGithub,
+            link: 'https://github.com/Ov3r1d3',
+            id: 's-github'
+        },        {
+            icon: faInstagram,
+            link: 'https://www.instagram.com/s.rouselakis/',
+            id: 's-instagram'
+        },
+        {
+            icon: faTwitter,
+            link: 'https://twitter.com/rouselakis',
+            id: 's-twitter'
         }
     ],
     photoFull: '/images/stelios_full.jpg',
@@ -19,18 +33,18 @@ export const SteliosRouselakis = {
 export const YannisRouselakis = {
     name: 'Dr. Yannis Rouselakis',
     jobTitle: 'Senior Cryptographic Developer, NTT Research',
-    websiteUrl: 'y.rouselakis.com',
+    websiteUrl: 'https://y.rouselakis.com',
     description: "I am a cryptographer with primary research interests in pairing-based cryptography, identity- and attribute-based based encryption. I am also interested in Quantum Computing, Computational Complexity, and Algorithms.",
     socials: [
+        {
+            icon: faLinkedin,
+            link: 'https://www.linkedin.com/in/yannis-rouselakis-a71643b1/',
+            id: 'linked-in'
+        },
         {
             icon: faGithub,
             link: 'https://github.com/yrouselakis',
             id: 'github'
-        },
-        {
-            icon: faLinkedinIn,
-            link: 'https://www.linkedin.com/in/yannis-rouselakis-a71643b1/',
-            id: 'linked-in'
         }
     ],
     photoFull: '/images/yannis_full.jpg',
